f67f0386e52c1df0cf41e0cd2a05b7d46ea4c804
lyan <jinhexi2@gmail.com>
2022-11-13 18:53:04 +0800

    split the antigen and antibody sequence

---
+++ feature.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import torch
+import numpy as np
+from tape import ProteinBertModel, TAPETokenizer
+from tqdm import tqdm
+
+
+def get_feature(_list):
+    # load model
+    # model = ProteinBertModel.from_pretrained('bert-base')
+    # torch.save(model, 'pretrain_bert.models')
+    model = torch.load('pretrain_bert.models', map_location='cpu')
+    for param in model.parameters():
+        param.requires_grad = False
+    model.eval()
+    tokenizer = TAPETokenizer(vocab='iupac')  # iupac is the vocab for TAPE models, use unirep for the UniRep model
+
+    feature = []
+    token_ids = 0
+    i =0
+    for seq in tqdm(_list):
+        seq = seq.split(";")
+        # if isinstance(seq, list):
+        cur_feature =[]
+        if len(seq) >1:
+            i=i+1
+        for cur_seq in seq:
+            token_ids = torch.tensor([tokenizer.encode(cur_seq)])
+            output = model(token_ids)
+            pooled_output = output[1]
+            cur_feature.append(pooled_output)
+        feature.append(torch.cat(cur_feature, dim=0).mean(dim =0).tolist())
+        # else:
+        #     token_ids = torch.tensor([tokenizer.encode(seq)])
+        #     output = model(token_ids)
+        #     pooled_output = output[1]
+        #     feature.append(pooled_output[0].tolist())
+
+    _df = pd.DataFrame(np.array(feature))
+    print(i)
+    return _df
+
+
+if __name__ == '__main__':
+    # 提取特征时全部提取
+    df = pd.read_csv('./data/Merge_aff_train_noE.csv', sep=',')
+    sequence = df['Sequence'].astype(str).tolist()
+    df = get_feature(sequence)
+    df.to_csv('./data/Merge_feature_aff_noE.csv', index=False)
+    df2 = pd.read_csv('./data/Merge_Neu_train_noE.csv', sep=',')
+    sequence = df2['Sequence'].astype(str).tolist()
+    df2 = get_feature(sequence)
+
+
+    df2.to_csv('./data/Merge_feature_neu_noE.csv', index=False)
